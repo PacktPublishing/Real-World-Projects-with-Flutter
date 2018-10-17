f2f3bea6991eb67d06ddc89674fd74dfc5c2a318
Nigel Henshaw <nigel@mobapptuts.com>
2018-10-17 13:18:55 +0900

    Working Mockup of Weather App

---
+++ src/section1/section1.1/my_weather_app/lib/ui/MyHomePage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:my_weather_app/ui/Weather.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: Weather(),
+    );
+  }
+}
