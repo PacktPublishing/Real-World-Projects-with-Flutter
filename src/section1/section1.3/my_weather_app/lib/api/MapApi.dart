0f5cdf2980716fca32db18d3b43a42e1f999f389
Nigel Henshaw <nigel@mobapptuts.com>
2018-10-19 21:25:49 +0900

    Removed the api key

--- src/section1/section1.3/my_weather_app/lib/api/MapApi.dart
+++ src/section1/section1.3/my_weather_app/lib/api/MapApi.dart
@@ -3,7 +3,8 @@ import 'package:my_weather_app/model/WeatherData.dart';
 
 class MapApi {
 
-  static const _apiKey = '011b68ffa175baa0ce29a8a87e6ffa54';
+  // Add your http://openweathermap.org api key here
+  static const _apiKey = '';
   static const _endpoint = 'http://api.openweathermap.org/data/2.5';
   double lat, lon;
   Client client = Client();
