53e6d3fd020bc477340cb493d4411796e3642067
Nigel Henshaw <nigel@mobapptuts.com>
2018-10-23 08:38:55 +0900

    Added unit test for location data

---
+++ src/section1/section1.5/my_weather_app/test/location_data_test.dart
@@ -0,0 +1,19 @@
+import 'package:my_weather_app/model/LocationData.dart';
+import 'package:test/test.dart';
+
+main() {
+
+  test('Test Location Data', () {
+    final double lat = 51.15;
+    final double lon = -0.13;
+
+    Map<String, double> testData = {
+      'latitude': lat,
+      'longitude': lon,
+    };
+
+    var locationData = LocationData.locationItems(testData);
+    expect(locationData.lat, equals(lat));
+    expect(locationData.lon, equals(lon));
+  });
+}
