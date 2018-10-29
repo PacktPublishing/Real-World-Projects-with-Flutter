4b28f20792b1ebc196da6f64b1e17a8b50fb8352
Nigel Henshaw <nigel@mobapptuts.com>
2018-10-30 08:56:52 +0900

    Completed section2.2 displaying map of local location

--- src/section2/section2.2/my_coffee_app/lib/widgets/MyMapPage.dart
+++ src/section2/section2.2/my_coffee_app/lib/widgets/MyMapPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:my_coffee_app/api/MyLocationApi.dart';
+import 'package:my_coffee_app/model/MyLocationData.dart';
 
 class MyMapPage extends StatefulWidget {
   MyMapPage({Key key, this.title}) : super(key: key);
@@ -13,6 +15,23 @@ class MyMapPage extends StatefulWidget {
 class _MyMapPageState extends State<MyMapPage> {
 
   GoogleMapController mapController;
+  MyLocationData _myLocationData;
+
+  Future<MyLocationData> _getLocation() async {
+    final locationApi = MyLocationApi.getInstance();
+    return await locationApi.getLocation();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getLocation().then((location) {
+      setState(() {
+        _myLocationData = location;
+      });
+    });
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     setState(() {
@@ -26,10 +45,22 @@ class _MyMapPageState extends State<MyMapPage> {
         title: new Text(widget.title),
       ),
       body: new Center(
-        child: SizedBox(
+        child: _myLocationData != null ? SizedBox(
           child: GoogleMap(
             onMapCreated: _onMapCreated,
+            options: GoogleMapOptions(
+              cameraPosition: CameraPosition(
+                  target: LatLng(
+                    _myLocationData.lat,
+                    _myLocationData.lon,
+                  ),
+                zoom: 14.0,
+              ),
+            ),
           ),
+        ) : CircularProgressIndicator(
+          strokeWidth: 4.0,
+          valueColor: AlwaysStoppedAnimation(Colors.white),
         ),
       ),
     );
