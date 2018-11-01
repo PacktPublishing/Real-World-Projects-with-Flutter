12a75abcd6172c751c7440d81bfde8cbe53ed550
Nigel Henshaw <nigel@mobapptuts.com>
2018-11-02 08:26:55 +0900

    added coffee card widget

---
+++ src/section2/section2.5/my_coffee_app/lib/widgets/CoffeeCard.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:my_coffee_app/config/Config.dart';
+
+class CoffeeCard extends StatelessWidget {
+
+  CoffeeCard({this.shopName, this.shopImage});
+
+  final String shopImage;
+  final String shopName;
+  static const _endpoint =
+      'https://maps.googleapis.com/maps/api/place/photo';
+
+  String _placesPhotoApi() {
+    return _endpoint + '?maxHeight=150&photoreference=' +
+      shopImage + '&key=' + apiKey;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 220.0,
+      width: 300.0,
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            Image.network(
+              _placesPhotoApi(),
+              height: 150.0,
+              width: 300.0,
+              fit: BoxFit.cover,
+            ),
+            Text(shopName),
+          ],
+        ),
+      ),
+    );
+  }
+}
