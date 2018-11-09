8a3ff5082b5e2f5d39a4ebb0c9854ccc56eb0854
Nigel Henshaw <nigel@mobapptuts.com>
2018-11-09 10:00:06 +0900

    Add new home page widget

---
+++ src/section3/section3.1/coffee_store_app/lib/widget/StoreHomePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StoreHomePage extends StatelessWidget {
+
+  final String title;
+
+  const StoreHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
