3029499d691cab88ba51a5702dbfbfec9441c33c
Nigel Henshaw <nigel@mobapptuts.com>
2018-11-29 09:11:31 +0900

    Added the create widget

---
+++ src/section4/section4.1/cafe_scanner_app/lib/widget/Create.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Create extends StatefulWidget {
+  @override
+  _CreateState createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+
+      ],
+    );
+  }
+}
