81c6936e2f59790dcf738e4784ee71d69391605f
Nigel Henshaw <nigel@mobapptuts.com>
2018-12-07 08:14:34 +0900

    Completed image captured portion

--- src/section4/section4.4/cafe_scanner_app/lib/widget/Scan.dart
+++ src/section4/section4.4/cafe_scanner_app/lib/widget/Scan.dart
@@ -1,4 +1,6 @@
+import 'package:cafe_scanner_app/api/FireStorage.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 class Scan extends StatefulWidget {
   @override
@@ -6,8 +8,51 @@ class Scan extends StatefulWidget {
 }
 
 class _ScanState extends State<Scan> {
+
+  List<CameraDescription> cameras;
+  CameraController controller;
+  bool isCameraInitialized = false;
+  String _imagePath;
+
+  void _initializeController() {
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if(!mounted)
+        setState(() {
+          isCameraInitialized = true;
+        });
+    });
+  }
+
+  _getCameras() async {
+    cameras = await availableCameras();
+    if(cameras.length > 0)
+      _initializeController();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCameras();
+  }
+
+  Future<String> saveQrCode() async {
+    final String filePath = await FireStorage.getFilePath();
+    if(controller.value.isTakingPicture)
+      return null;
+    try {
+      await controller.takePicture(filePath);
+    } on CameraException catch (e) {
+      throw Exception('Failed to capture image: $e');
+    }
+    return filePath;
+  }
+
   @override
   Widget build(BuildContext context) {
+    if(!isCameraInitialized)
+      return Container();
+
     return Column(
       children: <Widget>[
         Container(
@@ -20,11 +65,9 @@ class _ScanState extends State<Scan> {
                 fit: BoxFit.fitWidth,
                 child: Container(
                   width: 250,
-                  height: 250,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                    ),
+                  height: 250/controller.value.aspectRatio,
+                  child: CameraPreview(
+                    controller
                   ),
                 ),
               ),
