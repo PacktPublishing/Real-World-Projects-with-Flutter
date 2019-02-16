57cec52591f5d5faa9c72c73f09c0dc16c9246a7
Nigel Henshaw <nigel@mobapptuts.com>
2019-02-16 13:26:49 +0900

    Using specific package versions

--- src/section2/section2.5/my_coffee_app/lib/widgets/MyMapPage.dart
+++ src/section2/section2.5/my_coffee_app/lib/widgets/MyMapPage.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:my_coffee_app/api/CoffeeShopsApi.dart';
@@ -19,9 +17,8 @@ class MyMapPage extends StatefulWidget {
 
 class _MyMapPageState extends State<MyMapPage> {
 
-  CameraPosition _kInitialPosition;
   GoogleMapController mapController;
-  static MyLocationData _myLocationData;
+  MyLocationData _myLocationData;
   CoffeeShopsData _shops;
   Marker _selectedMarker;
   String _shopName;
@@ -71,7 +68,7 @@ class _MyMapPageState extends State<MyMapPage> {
 
   Future<CoffeeShopsData> _getCoffeeShops() async {
     final shopsApi = CoffeeShopsApi.getInstance();
-    return await shopsApi.getCoffeeShops(_myLocationData);
+    return await shopsApi.getCoffeeShops(this._myLocationData);
   }
 
   Future<MyLocationData> _getLocation() async {
@@ -86,13 +83,6 @@ class _MyMapPageState extends State<MyMapPage> {
     _getLocation().then((location) {
       setState(() {
         _myLocationData = location;
-        _kInitialPosition = CameraPosition(
-          target: LatLng(
-            _myLocationData.lat,
-            _myLocationData.lon,
-          ),
-          zoom: 14,
-        );
       });
     });
   }
@@ -117,7 +107,15 @@ class _MyMapPageState extends State<MyMapPage> {
             child: _myLocationData != null ? SizedBox(
               child: GoogleMap(
                 onMapCreated: _onMapCreated,
-                initialCameraPosition: _kInitialPosition,
+                options: GoogleMapOptions(
+                  cameraPosition: CameraPosition(
+                    target: LatLng(
+                      _myLocationData.lat,
+                      _myLocationData.lon,
+                    ),
+                    zoom: 14.0,
+                  ),
+                ),
               ),
             ) : CircularProgressIndicator(
               strokeWidth: 4.0,
